75da1844637897d89699be554b8c574ee4bf561e
Aybolek Hudayberdiyeva <ahudayberdiyeva@signifyhelth.com>
2024-05-05 21:37:02 -0500

    exception handling and swagger

--- src/main/java/com/aya/controller/UserController.java
+++ src/main/java/com/aya/controller/UserController.java
@@ -4,7 +4,6 @@ import com.aya.annotation.DefaultExceptionMessage;
 import com.aya.dto.UserDTO;
 import com.aya.entity.ResponseWrapper;
 import com.aya.exception.TicketingProjectException;
-import com.aya.service.RoleService;
 import com.aya.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
